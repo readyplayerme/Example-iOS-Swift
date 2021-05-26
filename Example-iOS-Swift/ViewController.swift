8ec21d2dca6437d280e2d4a5640582db8098d25f
Harrison Hough <harrisonhough4@gmail.com>
2021-05-26 14:48:35 +0300

    base xcode project setup

---
+++ Example-iOS-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example-iOS-Swift
+//
+//  Created by Harrison Hough on 26/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
