729ef01ad5b54fbb686d5ea18b6abd357aca421b
Harrison Hough <harrisonhough4@gmail.com>
2021-05-26 14:58:09 +0300

    added web view demo code

--- Example-iOS-Swift/ViewController.swift
+++ Example-iOS-Swift/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        let alert = UIAlertController(title: "Avatar URL Generated", message: "\(message.body)", preferredStyle: .alert)
+            
+             let okButton = UIAlertAction(title: "OK", style: .default, handler: { action in
+             })
+             alert.addAction(okButton)
+             DispatchQueue.main.async(execute: {
+                self.present(alert, animated: true)
+        })
+    }
+    
+    var webView: WKWebView!
+    
+    override func loadView(){
+        let config = WKWebViewConfiguration()
+        let source = "window.addEventListener('message', function(event){ document.querySelector('.content').remove(); setTimeout(() => {window.webkit.messageHandlers.iosListener.postMessage(event.data);}, 1000) });"
+        let script = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        config.userContentController.addUserScript(script)
+        config.userContentController.add(self, name: "iosListener")
+        webView = WKWebView(frame: UIScreen.main.bounds, configuration: config)
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://readyplayer.me/")! //Update to your custom URL here
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
